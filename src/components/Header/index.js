2836cc84bfc6aa809267c022b415cd3361ab2c80
joaoptgaino <joaoptgaino@hotmail.com>
2020-05-04 23:44:57 -0300

    second commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+/* class Header extends Component{
+    render(){
+        return <h1>blabla</h1>
+    }
+}
+Vai utilizar essa quando estiver no conceito de estado..
+*/
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+export default Header;
